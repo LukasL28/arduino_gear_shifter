c9934bf31e01f519311264329dac5bdd122f391b
Lukas <57566886+LukasL28@users.noreply.github.com>
2022-05-14 23:37:52 +0200

    added pin desc

--- arduino_programm/Jan.ino
+++ arduino_programm/Jan.ino
@@ -6,6 +6,15 @@
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
+// define relay's 
+#define MB1_1 2 // MM1 out
+#define MB1_2 3 // MM1 mid
+#define MB1_3 4 // MM1 in
+#define MB2_1 5 // MM2 out
+#define MB2_2 6 // MM2 mid
+#define MB2_3 7 // MM2 in
+#define MB3_1 8 // MM3
+
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
@@ -25,57 +34,67 @@ void setup() {
   display.setTextColor(WHITE);
   // Display static text
   
-  //pinMode(2, OUTPUT);
-  //pinMode(3, OUTPUT);
-  //pinMode(4, OUTPUT);
-  //pinMode(5, OUTPUT);
-  //pinMode(6, OUTPUT);  
-  //pinMode(7, OUTPUT);
-  //pinMode(8, OUTPUT);
-  //pinMode(9, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);  
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+
 }
 
 void loop() {
 
-      display.setCursor(2,85);
-  display.clearDisplay();
-  display.println("R");
-  display.display();
-  delay(500);
-  display.setCursor(2,85);
-  display.clearDisplay();
-  display.println("N");
-  display.display();
-  delay(500);
-  display.setCursor(2,85);
-  display.clearDisplay();
-  display.println("1");
-  display.display();
-  delay(500);
-  display.setCursor(2,85);
-  display.clearDisplay();
-  display.println("2");
-  display.display();
-  delay(500);
-  display.setCursor(2,85);
-  display.clearDisplay();
-  display.println("3");
-  display.display();
-  delay(500);
-  display.setCursor(2,85);
-  display.clearDisplay();
-  display.println("4");
-  display.display();
-  delay(500);
-  display.setCursor(2,85);
-  display.clearDisplay();
-  display.println("5");
-  display.display();
-  delay(500);
-  display.setCursor(2,85);
-  display.clearDisplay();
-  display.println("6");
-  display.display();
-  delay(500);
 
+
+}
+
+void gear_display(long number) {
+  display.setCursor(2,85);
+  display.clearDisplay();
+  display.println(number);
+  display.display();
+}
+
+void gears(long gear){
+    switch (gear)
+    {
+    case 0:
+
+      break;
+    case 1:
+      
+      break;
+    case 2:
+
+      break;
+
+    case 3:
+      
+        break;
+
+    case 4:
+        
+          break;
+
+    case 5:
+        
+          break;
+
+    case 6:
+
+          break;
+
+    default:
+      digitalWrite(MB3_1, HIGH);
+      digitalWrite(MB1_1, HIGH);
+      digitalWrite(MB1_2, LOW);
+      digitalWrite(MB1_3, HIGH);
+      digitalWrite(MB2_1, HIGH);
+      digitalWrite(MB2_2, LOW);
+      digitalWrite(MB2_3, HIGH);
+      digitalWrite(MB3_1, LOW);
+      break;
+    }
 }
